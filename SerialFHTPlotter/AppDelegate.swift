a50c4df29ac1a732b2089b9f9da22f7054cb0f59
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2016-08-23 21:27:38 -0700

    Initial Commit

---
+++ SerialFHTPlotter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SerialFHTPlotter
+//
+//  Created by Nevyn Bengtsson on 2016-08-23.
+//  Copyright © 2016 ThirdCog. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SerialFHTPlotter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SerialFHTPlotter
+//
+//  Created by Nevyn Bengtsson on 2016-08-23.
+//  Copyright © 2016 ThirdCog. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
